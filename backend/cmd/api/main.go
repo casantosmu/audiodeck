ecd08994938995faf9852c421686a5b5e14797c4
Carlos Santos <casantosmu@gmail.com>
2025-09-05 07:09:05 +0200

    Init go backend

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+// const version = "0.0.0"
+
+type config struct {
+	port     int
+	mediaDir string
+}
+
+// type application struct {
+// 	config config
+// 	logger *slog.Logger
+// }
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "Server port")
+	flag.StringVar(&cfg.mediaDir, "media-dir", "/", "Root directory for media files")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// app := &application{
+	// 	config: cfg,
+	// 	logger: logger,
+	// }
+
+	mux := http.NewServeMux()
+	// HANDLERS
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("starting server", "addr", srv.Addr, "media_dir", cfg.mediaDir)
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/casantosmu/audiodeck
+
+go 1.25.1
